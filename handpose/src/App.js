3d771ffa7ed6f47dcfe677f427e9862f42e9ade2
ashlynkc <ashchuparkoff@gmail.com>
2023-03-14 16:14:07 -0400

    Gesture recognition added
    Two gestures, one thumbs up and one peace sign

--- handpose/src/App.js
+++ handpose/src/App.js
@@ -1,30 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useRef} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Webcam from "react-webcam";
+
+// Imports for hand tracking
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose"
-import {drawHand} from "./utilities";
+import { drawHand } from "./utilities";
 
-const WebcamComponent = () => <Webcam />;
+// Imports for gesture tracking
+import * as fp from "fingerpose";
+import peace from "./peace.png";
+import thumbsUp from "./thumbsup.png"
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  // Emoji use states
+  const [emoji, setEmoji] = useState(null);
+  const images = {thumbsUp:thumbsUp, peace:peace};
+
   const runHandpose = async () => {
-
-    const net = await handpose.load()
-    console.log('Model loaded.')
-
-    // Loop and detect hands using setInterval
-    setInterval( () => {
-      // detect the handpose neural network
-      detect(net)
-
-    }, 100) // every 100ms, detect hand in frame
-
-  }
+    const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
 
   // "net" is actually our neural network from handpose.
   const detect = async (net) => {
@@ -53,15 +56,53 @@ function App() {
           const hand = await net.estimateHands(video);
           console.log(hand);
 
+          // Append gesture detection from fingerpose library of gestures
+          if (hand.length > 0) {
+
+            // Set up a gesture estimator (fingerpose)
+            const GE = new fp.GestureEstimator ([
+
+              fp.Gestures.VictoryGesture, // peace sign
+              fp.Gestures.ThumbsUpGesture,
+
+            ])
+
+            const gesture = await GE.estimate(hand[0].landmarks, 8);
+            console.log(gesture);
+
+            if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+
+              // predict most confident gesture
+              const confidence = gesture.gestures.map(
+
+                (prediction) => prediction.score
+
+              );
+
+              const maxConfidence = confidence.indexOf(
+
+                Math.max.apply(null, confidence)
+
+              );
+              console.log(maxConfidence)
+
+              setEmoji(gesture.gestures[maxConfidence].name);
+    
+    
+
+            }
+
+          }
+
           // Draw the hand mesh.
           const ctx = canvasRef.current.getContext("2d");
           drawHand(hand, ctx);
 
         }
 
-  }
+  };
 
-  runHandpose();
+useEffect(() => { runHandpose() } , []);
 
   return (
     
@@ -104,6 +145,32 @@ function App() {
           }}
         
         />
+
+        {emoji !== null ? (
+
+          <img
+
+            src = {images[emoji]}
+            style = {{
+
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 400,
+              bottom: 500,
+              right: 0,
+              textAlign: "center",
+              height: 100,
+            
+            }}
+
+          />
+
+        ) : (
+
+          ""
+
+        )}
 
       </header>
 
